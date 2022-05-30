3386971a10de0ecca137ae9c8bac8a0342a10fc4
N7i7k7i7t7a7 <nikitapund01@gmail.com>
2022-05-30 19:25:10 +0530

    first branch 'iss1' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,10 +1,15 @@
 package com.cts;
 
+import com.cts.service.SomeService;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to session");
 		System.out.println("Hope you are able to follow?");
+		
+		SomeService ss=new SomeService();
+		System.out.println(ss.findLength("program"));
 	}
 
 }
